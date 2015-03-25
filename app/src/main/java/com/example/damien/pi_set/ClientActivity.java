faf959d7578d8950dc9f99094a9d7e2578c8a28e
X-Boris <borismuzellec@free.fr>
2015-03-25 22:28:22 +0100

    Client qui marche pas

--- app/src/main/java/com/example/damien/pi_set/ClientActivity.java
+++ app/src/main/java/com/example/damien/pi_set/ClientActivity.java
@@ -4,15 +4,73 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import java.lang.Integer;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 public class ClientActivity extends ActionBarActivity {
+
+    // DEFAULT IP
+    public static String SERVERIP = "10.0.2.15";
+
+    // DESIGNATE A PORT
+    public static int SERVERPORT = 8080;
+
+    private Socket clientSocket;
+
+
+    static Socket establishConnection(String ip, int port) {
+        try {
+            return new Socket(ip, port);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException("Impossible de resoudre l'adresse");
+        } catch (IOException e) {
+            throw new RuntimeException("Impossible de se connecter a l'adresse");
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+
+        Button connection = (Button) findViewById(R.id.connection);
+        final EditText editIP = (EditText) findViewById(R.id.IP);
+        final EditText editPort = (EditText) findViewById(R.id.port);
+        connection.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                new ClientThread().start();
+
+            }
+
+        });
+
+
     }
+
+
+    public class ClientThread extends Thread {
+
+        final EditText editIP = (EditText) findViewById(R.id.IP);
+        final EditText editPort = (EditText) findViewById(R.id.port);
+
+        public void run() {
+            SERVERIP = editIP.getText().toString();
+            SERVERPORT = Integer.parseInt(editPort.getText().toString());
+
+            establishConnection(SERVERIP, SERVERPORT);
+        }
+    }
+
 
 
     @Override
